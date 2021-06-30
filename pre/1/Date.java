4876f13b795ed05cd0f03792401cb7bd2a4ab51c
Lyuba Fridman <lyuba.fridman@gmail.com>
2021-06-30 11:21:12 -0400

    Copying pre-work 1st assignment

---
+++ pre/1/Date.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Date {
+    public static void main(String[] args){
+        String day = "Saturday"; 
+		System.out.println(day);
+		int date = 12;
+		System.out.println(date);
+		String month = "June";
+		System.out.println(month);
+		int year = 2021;
+		System.out.println(year);
+    }
+}
