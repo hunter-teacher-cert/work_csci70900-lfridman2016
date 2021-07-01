2e81823a8cd219ce3b77f38f718899b8994c8645
Lyuba Fridman <lyuba.fridman@gmail.com>
2021-07-01 11:57:46 -0400

    Working print methods

--- 3/skel/TwoDimArray.java
+++ 3/skel/TwoDimArray.java
@@ -10,47 +10,61 @@ public class TwoDimArray
   //print each row of 2D integer array a on its own line,
   // using a FOR loop
   public static void print1( int[][] a ) {
-
+	for(int i = 0; i < a.length; i++)
+	{
+		for(int j = 0; j < a[i].length; j++)
+		{
+			System.out.print(a[i][j] + " ");
+		}
+		System.out.println();
+	}
   }
 
 
   //print each row of 2D integer array a on its own line,
   // using a FOREACH loop
   public static void print2( int[][] a ) {
-
+	for(int[] arr1d: a)
+	{
+		for(int elem: arr1d)
+		{
+			System.out.print(elem + " ");
+		}
+		System.out.println();
+	}
   }
 
 
   //return sum of all items in 2D integer array a
   public static int sum1( int[][] a ) {
-
+	return 0;
   }
 
 
   //return sum of all items in 2D integer array a
   // using helper fxn sumRow
   public static int sum2( int [][] m ) {
-
+	return 0;
   }
 
 
   //return sum of all items on row r of 2D integer array a
   // using a FOR loop
   public static int sumRow( int r, int[][] a ) {
-
+	return 0;
   }
 
 
   //return sum of all items on row r of 2D integer array a
   // using a FOREACH loop
   public static int sumRow2( int r, int[][] m ) {
-
+	return 0;
   }
 
 
   public static void main( String [] args )
   {
-		/* ~~~v~~~~~slide~me~down~as~you~test~~~~~~~~~~~~~~~v~~~
+		
        int [][] m1 = new int[4][2];
        int [][] m2 = { {2,4,6}, {3,5,7} };
        int [][] m3 = { {2}, {4,6}, {1,3,5} };
@@ -60,6 +74,7 @@ public class TwoDimArray
        print2(m1);
        print2(m2);
        print2(m3);
+	   /* ~~~v~~~~~slide~me~down~as~you~test~~~~~~~~~~~~~~~v~~~
        System.out.print("testing sum1...\n");
        System.out.println("sum m1 : " + sum1(m1));
        System.out.println("sum m2 : " + sum1(m2));
