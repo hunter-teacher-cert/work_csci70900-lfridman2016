413bc8067c38601cbc707ba21efd886943c43144
Lyuba Fridman <lyuba.fridman@gmail.com>
2021-07-01 12:36:56 -0400

    TwoDimArray completed

--- 3/skel/TwoDimArray.java
+++ 3/skel/TwoDimArray.java
@@ -37,28 +37,49 @@ public class TwoDimArray
 
   //return sum of all items in 2D integer array a
   public static int sum1( int[][] a ) {
-	return 0;
+	int total = 0;
+	for(int i = 0; i < a.length; i++)
+	{
+		total += sumRow(i, a);
+	}
+	return total;
   }
 
 
   //return sum of all items in 2D integer array a
   // using helper fxn sumRow
   public static int sum2( int [][] m ) {
-	return 0;
+	int total = 0;
+	for(int i = 0; i < m.length; i++)
+	{
+		total += sumRow2(i, m);
+	}
+	return total;
+		
   }
 
 
   //return sum of all items on row r of 2D integer array a
   // using a FOR loop
   public static int sumRow( int r, int[][] a ) {
-	return 0;
+	int total = 0;
+	for(int i = 0; i < a[r].length; i++)
+	{
+		total += a[r][i];
+	}
+	return total;
   }
 
 
   //return sum of all items on row r of 2D integer array a
   // using a FOREACH loop
   public static int sumRow2( int r, int[][] m ) {
-	return 0;
+	int total = 0;
+	for(int elem : m[r])
+	{
+		total += elem;
+	}
+	return total;
   }
 
 
@@ -74,7 +95,7 @@ public class TwoDimArray
        print2(m1);
        print2(m2);
        print2(m3);
-	   /* ~~~v~~~~~slide~me~down~as~you~test~~~~~~~~~~~~~~~v~~~
+
        System.out.print("testing sum1...\n");
        System.out.println("sum m1 : " + sum1(m1));
        System.out.println("sum m2 : " + sum1(m2));
@@ -83,6 +104,7 @@ public class TwoDimArray
        System.out.println("sum m1 : " + sum2(m1));
        System.out.println("sum m2 : " + sum2(m2));
        System.out.println("sum m3 : " + sum2(m3));
+	  /* ~~~v~~~~~slide~me~down~as~you~test~~~~~~~~~~~~~~~v~~~
        ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~*/
   }//end main()
 
