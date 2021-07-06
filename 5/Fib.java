fbb9c8a3f9e9e167baeb88bb15f9e9804836c0ce
Lyuba Fridman <lyuba.fridman@gmail.com>
2021-07-06 12:48:34 -0400

    Using arrays for a more efficient algorithm

--- 5/Fib.java
+++ 5/Fib.java
@@ -1,7 +1,9 @@
+/** Marina, Chris, Lyuba **/
+import java.util.Arrays;
 // heading
 
 public class Fib {
-
+	static int[] f=new int[100];
   //your implementation of fib(n) here
 	public static int fib (int n){
 		int fibNum;
@@ -12,14 +14,42 @@ public class Fib {
 		return fibNum;
 	}
 	
+	public static void fibArrayAssignment(){
+		for (int i=0; i<f.length; i++){
+			f[i]=-1;
+		}
+	}
+	
+	
+	public static int fibArray (int n){
+		int fibNum;
+		if (f[n]>-1){
+			return f[n];
+		}
+		else{
+			if (n==0 || n==1){	//base case
+			f[n]=n;
+			return n;
+		}
+		f[n]=fibArray(n-1)+fibArray(n-2);
+		return f[n];
+		}
+	}		
+	
+	
+	
 
   public static void main( String[] args ) {
-	
-	
+	  fibArrayAssignment();
+	  System.out.println(fibArray(5));
+	  System.out.println(Arrays.toString(f));
 
 	
 	
 
+	
+	
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     System.out.println( fib(0) ); // -> 0
     System.out.println( fib(1) ); // -> 1
@@ -32,9 +62,10 @@ public class Fib {
     System.out.println( fib(10) ); // -> 55
     System.out.println( fib(20) ); // -> 6765
     System.out.println( fib(40) ); // -> 102334155
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
   }//end main()
+
 
 }//end class Fib
