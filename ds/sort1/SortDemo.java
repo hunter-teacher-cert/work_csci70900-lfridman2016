b773c8c94b14c0fff034a31e07012b4412f42376
Lyuba Fridman <lyuba.fridman@gmail.com>
2021-07-09 11:31:43 -0400

    Working sort

--- ds/sort1/SortDemo.java
+++ ds/sort1/SortDemo.java
@@ -7,10 +7,10 @@ Setup:
  1.Make a folder under your work repo named: ds
  2. Make another folder under that named sort1
  3. Copy this file and SortDemoDriver.java into the ds/sort1 folder
- 
+
 Lab:
 
-Part 1:  
+Part 1:  X
   1. Analyze the two constructors - try to figure out how they work.
   2. Compile and run the program (SortDemoDriver.java and SortDemo.java) and confirm
    the behavior of the constructors.
@@ -29,27 +29,30 @@ public class SortDemo{
 
     /* Instance Variables */
     private ArrayList<Integer> data;  // to store the data
-    private Random r; 
+    private Random r;
 
-    
+    //Constructor 1 - Loads 15 random numbers into data.
     public SortDemo(){
-	data = new ArrayList<Integer>();
-	r = new Random();
-	for (int i=0;i<15;i++){
-	    data.add(r.nextInt(20));
-	}
-	
-    }
-    
-    public SortDemo(int size){
-	data = new ArrayList<Integer>();
-	r = new Random();
-	for (int i=0;i<size;i++){
-	    data.add(r.nextInt(20));
-	}
-	
-    }
+	data = new ArrayList<Integer>(); //initialize the private ArrayList data
+	r = new Random();  //initializing a Random object so that it can generate random numbers
+	for (int i=0;i<15;i++){ //loop from 0 to 14 (inclusive), increments by 1
+	    data.add(r.nextInt(20)); //generates a random number from 0 to 20 and adds it to the ArrayList data
+	} //end for
 
+    } //end SortDemo()
+
+	//Constructor 2 - SortDemo with parameter size - Loads size random integers to data
+    public SortDemo(int size){ //takes parameter size
+	data = new ArrayList<Integer>(); //initializing data ArrayList
+	r = new Random(); //initializing Random object
+	for (int i=0;i<size;i++){ //loop from 0 to size - 1 (inclusive) increments by 1
+	    data.add(r.nextInt(20));  //generates a random number from 0 to 20 and adds it to the ArrayList data
+	} //end for
+
+    } //end SortDemo(int size)
+  public int get(int index){
+    	return this.data.get(index);
+        }
     /*
       return the index of the ArrayList data from index start to the end
       Example, if the arraylist has:
@@ -60,29 +63,47 @@ public class SortDemo{
     */
     public int findSmallestIndex(int start){
 	int smallIndex = start;
-	
+
 	// start a variable at the one after start
 	// your code here
-	
+	for(int i = start; i < data.size(); i++)
+	{
 	// loop from that variable to end and update smallIndex as needed
+
+	//is the current number smaller than the smallest number so far?
+		if( data.get(i) < data.get(smallIndex))
+		{
+			//update the smallest index
+			smallIndex =  i;
+		} //end if
+	} //end for
 	// your code here
-	
+
 	return smallIndex;
 
+    } //end findSmallestIndex
+
+
+    // find the smallet index from i to end
+    // your code here
+
+    // swap the item at that index and i
+    // your code here
+    //go through the array ArrayList
+    //
+    public void swap(int Index1, int Index2){
+      int TempValue1 = data.get(Index1);
+      data.set(Index1,data.get(Index2));
+      data.set(Index2,TempValue1);
     }
-
-
-    
     public void sort(){
 	int i;
 	for (i=0;i < data.size()-1; i++){
-	    // find the smallet index from i to end
-	    // your code here
+    int tempSmallest = findSmallestIndex(i); //utilize previous method to find smallest index and assign it a local variable
+    swap(tempSmallest, i);
 
-	    // swap the item at that index and i
-	    // your code here
-	    
-	    
+
+
 	}
     }
 
@@ -93,22 +114,22 @@ public class SortDemo{
 	// loop through the ArrayList data
 	// and if the value you're searchign for is in the ArrayList, return it.
 	// return -1 if it isn't there.
-	
-	
+
+
 	return 0; // replace this return
     }
-    
+
     /* If you finish the lab early you can get started on this */
     public int binarySearch(int value){
 	boolean replacethiswithrealexpression=false;
 	int lowerIndex = 0;
 	int upperIndex = data.size();
 	int middleIndex = data.size()/2;
-	
+
 	/* if upper crosses lower it's not there and the lop should exit the loop
 	   and if the item is at middle you should exit the loop
-    
-           you have to replace the "replacethiswithrealexpression" boolean 
+
+           you have to replace the "replacethiswithrealexpression" boolean
            with a correct expression based on lowerIndex and upperIndex
 	*/
 	while (replacethiswithrealexpression)
@@ -118,17 +139,17 @@ public class SortDemo{
 		// from lower to the middle and otherwise from the middle to the upper.
 		//
 		// then update middleIndex based on new lowerIndex and upperIndex.
-	    
+
 	    }
-	
+
 	/* replace this return to either return the value if it was found and -1
 	   if upperIndex and lowerIndex crossed
-	*/    
-	
+	*/
+
 	return 0; // replace this return
     }
-    
-	
+
+
     public String toString(){
 	return ""+data;
     };
--- ds/sort1/SortDemoDriver.java
+++ ds/sort1/SortDemoDriver.java
@@ -4,27 +4,24 @@ import java.util.*;
 public class SortDemoDriver {
     public static void main(String[] args) {
 
-	SortDemo sd = new SortDemo();
+	SortDemo sd = new SortDemo(); //call to 1st constructor
 	System.out.println(sd);
 
-	SortDemo sd2 = new SortDemo(20);
+	SortDemo sd2 = new SortDemo(20); //call to 2nd constructor
 	System.out.println(sd2);
-		
-	/* part 2 test lines */
-	/*
-	  int i;
-	i = sd.findSmallestIndex();
-	System.out.println("sd["+"] = "+sd.get(i)+" : " + sd);
 
-	i = sd2.findSmallestIndex();
-	System.out.println("sd2["+"] = "+sd2.get(i)+" : " + sd2);
-	*/
-	
+	/* part 2 test lines */
+
+  int i;
+	i = sd.findSmallestIndex(0);
+	System.out.println("sd["+ i +"] = "+sd.get(i)+" : " + sd);
+
+	i = sd2.findSmallestIndex(0);
+	System.out.println("sd2["+ i + "] = "+sd2.get(i)+" : " + sd2);
 	/* part 3 test lines */
-	/*
+
 	  System.out.println(sd);
 	  sd.sort();
 	  System.out.println(sd);
-	*/
     }
 }
